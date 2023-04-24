b6cbc9211b56c75e22cca4dce5e90f6eb154d2fc
Santiago Reyes <sreyes@itba.edu.ar>
2023-04-24 14:48:05 -0300

    clase 4

--- clase-04/greetings/consumer/consumer.py
+++ clase-04/greetings/consumer/consumer.py
@@ -2,31 +2,34 @@ from nameko.standalone.rpc import ClusterRpcProxy
 from os import environ
 
 CONFIG = {
-    'AMQP_URI': f'amqp://{environ["RABBIT_USER"]}:{environ["RABBIT_PASSWORD"]}@{environ["RABBIT_HOST"]}:{environ["RABBIT_PORT"]}/'
+    "AMQP_URI": f'amqp://{environ["RABBIT_USER"]}:{environ["RABBIT_PASSWORD"]}@{environ["RABBIT_HOST"]}:{environ["RABBIT_PORT"]}/'
 }
 
+
 class Consumer:
-    name = 'consumer_service'
+    name = "consumer_service"
 
     def __init__(self, rpc):
-        self.response=[]
+        self.response = []
         self.rpc = rpc
-    
+
     def say_hello(self, name):
-        self.response.append( self.rpc.greeting_service.hello.call_async( name=name ) )
+        self.response.append(self.rpc.greeting_service.hello.call_async(name=name))
 
     def get_response(self):
-        return [ resp.result() for resp in self.response ]
+        return [resp.result() for resp in self.response]
+
 
 def main():
 
     with ClusterRpcProxy(CONFIG) as cluster_rpc:
         c = Consumer(cluster_rpc)
         for i in range(5):
-            c.say_hello( name=f'user_{i}' ) 
+            c.say_hello(name=f"user_{i}")
 
         for r in c.get_response():
             print(r)
 
-if __name__=='__main__':
+
+if __name__ == "__main__":
     main()
--- clase-04/greetings/service/service.py
+++ clase-04/greetings/service/service.py
@@ -2,13 +2,11 @@ from time import sleep
 from nameko.rpc import rpc
 from random import randint
 
+
 class GreetingService:
-    name = 'greeting_service'
+    name = "greeting_service"
 
     @rpc
     def hello(self, name):
-        sleep( randint(1,10) )
-        return f'Hello {name}'
-    
-
-    
+        sleep(randint(1, 10))
+        return f"Hello {name}"
